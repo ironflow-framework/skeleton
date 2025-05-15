f91657452eccef59394f01ba3fce7b8a476e74d3
Aure <147180124+AureDulvresse@users.noreply.github.com>
2025-05-15 19:41:31 +0100

    Create UserFactory.php

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use IronFlow\Database\Factory;
+use App\Models\User;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition(): array
+    {
+        return [
+            // 'first_name' => $this->faker->firstName(),
+            // 'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+        ];
+    }
+}
